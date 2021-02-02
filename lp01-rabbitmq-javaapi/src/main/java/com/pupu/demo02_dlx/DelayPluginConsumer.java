e9a59cac0f67a9c8429ab4f2dd2776f0f7a7a842
zn_lp <lipu@zgzhongnan.com>
2021-02-02 14:04:28 +0800

    使用延时插件实现的消息投递-生产者

--- lp01-rabbitmq-javaapi/src/main/java/com/pupu/demo02_dlx/DelayPluginConsumer.java
+++ lp01-rabbitmq-javaapi/src/main/java/com/pupu/demo02_dlx/DelayPluginConsumer.java
@@ -1,4 +1,4 @@
-package com.pupu.dlx;
+package com.pupu.demo02_dlx;
 
 import com.rabbitmq.client.*;
 
@@ -9,9 +9,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 使用延时插件实现的消息投递-消费者
- * 必须要在服务端安装rabbitmq-delayed-message-exchange插件，安装步骤见README.MD
- * 先启动消费者
+ *   使用延时插件实现的消息投递-消费者
+ *   必须要在服务端安装rabbitmq-delayed-message-exchange插件，安装步骤见README.MD
+ *   先启动消费者
  *
  * @author lp
  * @since 2021/2/1 23:05
@@ -21,29 +21,28 @@ public class DelayPluginConsumer {
     public static void main(String[] args) throws Exception {
 
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setUri("amqp://guest:guest@1114.55.95.30:5673");
+        factory.setUri("amqp://guest:guest@114.55.95.30:5673");
         Connection conn = factory.newConnection();
         Channel channel = conn.createChannel();
 
-        // 声明x-delayed-message类型的exchange
+        //1. 声明x-delayed-message类型的exchange
         Map<String, Object> argss = new HashMap<>();
         argss.put("x-delayed-type", "direct");
-        channel.exchangeDeclare("DELAY_EXCHANGE", "x-delayed-message", false,
-                false, argss);
+        channel.exchangeDeclare("DELAY_EXCHANGE", "x-delayed-message", false, false, argss);
 
-        // 声明队列
+        //2. 声明队列
         channel.queueDeclare("DELAY_QUEUE", false, false, false, null);
 
-        // 绑定交换机与队列
+        //3. 绑定交换机与队列
         channel.queueBind("DELAY_QUEUE", "DELAY_EXCHANGE", "DELAY_KEY");
 
-        // 创建消费者
+        //4. 创建消费者
         Consumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body){
                 String msg = new String(body, StandardCharsets.UTF_8);
                 SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-                System.out.println("收到消息：[" + msg + "]\n接收时间：" + sf.format(new Date()));
+                System.out.println("收到消息：[" + msg + "]\n接收时间：" + sf.format(new Date())+"\n");
             }
         };
 
--- lp01-rabbitmq-javaapi/src/main/java/com/pupu/demo02_dlx/DelayPluginProducer.java
+++ lp01-rabbitmq-javaapi/src/main/java/com/pupu/demo02_dlx/DelayPluginProducer.java
@@ -1,4 +1,4 @@
-package com.pupu.dlx;
+package com.pupu.demo02_dlx;
 
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
@@ -23,30 +23,30 @@ public class DelayPluginProducer {
     public static void main(String[] args) throws Exception {
 
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setUri("amqp://guest:guest@127.0.0.1:5672");
+        factory.setUri("amqp://guest:guest@114.55.95.30:5673");
         Connection conn = factory.newConnection();
         Channel channel = conn.createChannel();
 
-        // 延时投递，比如延时1分钟
+        //1. 延时投递，比如延时1分钟
         Date now = new Date();
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.SECOND, +10);//
         Date delayTime = calendar.getTime();
 
-        // 定时投递，把这个值替换delayTime即可
+        //2. 定时投递，把这个值替换delayTime即可
         // Date exactDealyTime = new Date("2019/01/14,22:30:00");
 
         SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         String msg = "发送时间：" + sf.format(now) + "，投递时间：" + sf.format(delayTime);
 
-        // 延迟的间隔时间，目标时刻减去当前时刻
+        //3. 延迟的间隔时间，目标时刻减去当前时刻
         Map<String, Object> headers = new HashMap<>();
         headers.put("x-delay", delayTime.getTime() - now.getTime());
 
-        AMQP.BasicProperties.Builder props = new AMQP.BasicProperties.Builder()
+        AMQP.BasicProperties.Builder props = new AMQP.BasicProperties
+                .Builder()
                 .headers(headers);
-        channel.basicPublish("DELAY_EXCHANGE", "DELAY_KEY", props.build(),
-                msg.getBytes());
+        channel.basicPublish("DELAY_EXCHANGE", "DELAY_KEY", props.build(), msg.getBytes());
 
         channel.close();
         conn.close();
